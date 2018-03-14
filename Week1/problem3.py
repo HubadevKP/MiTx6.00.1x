4b178761b15f842d76ee938d2875bd7155d131a8
HubadevKP <37345892+HubadevKP@users.noreply.github.com>
2018-03-14 17:00:14 +0000

    Update problem3.py

--- Week1/problem3.py
+++ Week1/problem3.py
@@ -9,16 +9,16 @@
 s = 'abcdcd'
 maxLen = 0
 current = s[0]
-longest = s[0]
+long = s[0]
 
 for i in range(len(s) - 1):
     if s[i + 1] >= s[i]:
         current += s[i + 1]
         if len(current) > maxLen:
             maxLen = len(current)
-            longest = current
+            long = current
     else:
         current = s[i + 1]
 
     i += 1
-print ('Longest substring in alphabetical order is: ' + longest)
+print ('Longest substring in alphabetical order is: ' + long)
