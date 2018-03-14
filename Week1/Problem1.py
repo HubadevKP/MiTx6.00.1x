1c3f4aca89a62e39e3f86eb42bb498cff0979985
HubadevKP <37345892+HubadevKP@users.noreply.github.com>
2018-03-14 12:32:32 +0000

    Update Problem1.py

--- Week1/Problem1.py
+++ Week1/Problem1.py
@@ -1,11 +1,12 @@
-# PROBLEM 1 from MiTx6.00.1x
-#
+# PROBLEM 1 from MiTx6.00.1x:
 # Assume s is a string of lower case characters.
 # Write a program that counts up the number of vowels contained in the string s.
 # Valid vowels are: 'a', 'e', 'i', 'o', and 'u'. For example, if s = 'azcbobobegghakl',
 # your program should print: Number of vowels:
 # There is str before count in last line becouse: "TypeError: Can't convert 'int' object to str implicitly"
+# count = 0 becouse we start counting from zero and adding 1 if we found letter what we intrest in.
 
+#PROBLEM1
 #  Variables for problem 1
 s = 'aaeiuoliujfdsaasdfoeiu'
 count = 0
@@ -16,12 +17,11 @@ for letter in s:
 print ('Number of vowels: ' + str(count))
 
 
-# Example1 to better understending:
+# EXAMPLE to better understending:
 # a = exemplary word
 # count = 0 becouse we start counting from zero and adding 1 if we found letter what we intrest in.
 
 # Variables for example
-
 a = 'Egg'
 count = 0
 # # Program for example
